60920c476c975959b316642d8b2bfbaa53af3645
brunojadami <brunojadami@4e934d8a-349f-96be-9c47-af5827d75f68>
2010-09-03 16:05:08 +0000

    Graphics: Renamed to Contexts, it will also have the Input and Timing implementations.

---
+++ Contexts/src/guitarjava/graphics/GraphicsUpdateListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package guitarjava.graphics;
+
+import java.util.EventObject;
+
+/**
+ *
+ * @author brunojadami
+ */
+public interface GraphicsUpdateListener
+{
+    public void handleUpdateEvent(EventObject e);
+}
