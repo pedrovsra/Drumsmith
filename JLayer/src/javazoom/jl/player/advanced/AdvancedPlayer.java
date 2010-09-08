041f6e36322ecd0013fea3f8ac14408aec2bf4eb
lucasjadami <lucasjadami@4e934d8a-349f-96be-9c47-af5827d75f68>
2010-09-08 19:40:41 +0000

    JLayer: Fixed small crash.

--- JLayer/src/javazoom/jl/player/advanced/AdvancedPlayer.java
+++ JLayer/src/javazoom/jl/player/advanced/AdvancedPlayer.java
@@ -69,6 +69,9 @@ public class AdvancedPlayer {
 
     public int getCurrentPosition()
     {
+        if (audio == null)
+            return 0;
+        
         return audio.getPosition();
     }
 
