7201d66c2157ae2681884852297c4af99ead77ae
lucasjadami <lucasjadami@4e934d8a-349f-96be-9c47-af5827d75f68>
2010-09-11 17:30:50 +0000

    GameBase: The guitar buttons now have many flames. The track extension was reduced by a fixed size.

--- trunk/GameBase/src/guitarjava/main/Main.java
+++ trunk/GameBase/src/guitarjava/main/Main.java
@@ -31,7 +31,7 @@ public class Main
         InputNoRepeatContext inputContext = new InputNoRepeatContext();
 
         GameEngine gameEngine = new GameEngine(graphicsContext, timingContext, inputContext,
-                gameWindow, new Music("musics/xml/Music3_4.xml", "musics/mp3/Music3.mp3"));
+                gameWindow, new Music("musics/xml/Music1_4.xml", "musics/mp3/Music1.mp3"));
 
         gameEngine.start();
     }
