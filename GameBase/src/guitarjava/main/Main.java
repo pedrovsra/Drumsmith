48291d4e957170d85cd0022ca301f8e4b1e5cc52
lucasjadami <lucasjadami@4e934d8a-349f-96be-9c47-af5827d75f68>
2010-09-14 23:56:05 +0000

    GameBase: Small changes.

--- GameBase/src/guitarjava/main/Main.java
+++ GameBase/src/guitarjava/main/Main.java
@@ -30,7 +30,7 @@ public class Main
         InputNoRepeatContext inputContext = new InputNoRepeatContext();
 
         GameEngine gameEngine = new GameEngine(graphicsContext, timingContext, inputContext,
-                gameWindow, new Music("musics/xml/Music5_4.xml", "musics/mp3/Music5.mp3"));
+                gameWindow, new Music("musics/xml/Music2_4.xml", "musics/mp3/Music2.mp3"));
 
         gameEngine.start();
     }
