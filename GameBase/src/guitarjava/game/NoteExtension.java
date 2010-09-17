f21b56eb8ed435ed5cfa40dd09cceda1343adc29
lucasjadami <lucasjadami@4e934d8a-349f-96be-9c47-af5827d75f68>
2010-09-17 11:37:55 +0000

    GameBase: A new class was created to change the beahaviour of the burning note extension.

--- GameBase/src/guitarjava/game/NoteExtension.java
+++ GameBase/src/guitarjava/game/NoteExtension.java
@@ -8,16 +8,18 @@ public class NoteExtension extends TrackObject
 {
     private static final int WIDTH = 16;
 
-    private boolean powning;
-    private BurningInterface burningState;
-
     /**
      * @param track Track of this object.
      * @param height Height of the extension.
      */
     public NoteExtension(int track, double height, double noteOriginY, double noteHeight)
     {
-        super(track, -(height / 2 + .5f*noteHeight) + noteOriginY, 0, WIDTH, height, -1);
+        this(track, -(height / 2 + .5f*noteHeight) + noteOriginY, height);
+    }
+
+    public NoteExtension(int track, double position, double height)
+    {
+        super(track, position, 0, WIDTH, height, -1);
 
         drawData.createAs2DFilledRect(WIDTH, (int) height);
     }
@@ -26,29 +28,7 @@ public class NoteExtension extends TrackObject
     public void think(float deltaTime)
     {
         y += TRACK_DEFAULT_SPEED * deltaTime;
-        
-        if (powning && y > TrackObject.BURNING_POSITION_Y - height / 2)
-        {
-            height = Math.max(0, height - TRACK_DEFAULT_SPEED * deltaTime);
-            drawData.createAs2DFilledRect(WIDTH, (int) height);
-
-            y -= TRACK_DEFAULT_SPEED * deltaTime / 2;
-
-            powning = burningState.isBurning();
-        }
 
         updateDrawDataPosition();
     }
-
-    /**
-     * @param burningState An interface that indicates if it needs to keep burning.
-     */
-    public void setPowned(BurningInterface burningState)
-    {
-        powning = true;
-        this.burningState = burningState;
-
-        drawData.createAs2DFilledRect((int) width, (int) height);
-    }
-
 }
